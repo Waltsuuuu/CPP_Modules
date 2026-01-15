58f8e0b708a57e8680012ec60fb410bc7114f153
Waltteri Heino <waltteriheino@gmail.com>
2026-01-15 16:36:39 +0200

    Removed unnecessary semicolons

--- cpp_01/ex01/Zombie.cpp
+++ cpp_01/ex01/Zombie.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 
 Zombie::Zombie() {
-};
+}
 
 Zombie::~Zombie() {
 	std::cout << _name << " destroyed\n";
-};
+}
 
 void Zombie::setZombieName( std::string name ) {
 	_name = name;
