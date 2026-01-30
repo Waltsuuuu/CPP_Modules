359873e550a556abc6c5d0a8be645ad6c5053444
Waltteri Heino <waltteriheino@gmail.com>
2026-01-30 15:59:48 +0200

    mod02 / ex00 - Orthodox Canonical form done

---
+++ cpp_02/ex00/Fixed.cpp
@@ -0,0 +1,23 @@
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed() : _value(0) {
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed &copy) : _value(copy._value){
+	std::cout << "Copy constructor called\n";
+}
+
+Fixed& Fixed::operator=(const Fixed& fixed) {
+	std::cout << "Copy assignemnt operator called\n";
+	// Self assignmnet guard
+	if (this != &fixed) {
+		this->_value = fixed._value;
+	}
+	return *this;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called\n";
+}
--- cpp_02/ex00/Fixed.hpp
+++ cpp_02/ex00/Fixed.hpp
@@ -3,23 +3,23 @@
 
 class Fixed {
 	private:
-		int 				_fixed;
-		static const int	_fracBits;
+		int 				_value;
+		static const int	_fracBits = 8;
 	public:
 		// Default constructor
 		Fixed();
 		// Copy constructor (Creates a new object as a copy of an existing one)
 		Fixed(const Fixed &copy);
 		//Copy assignment operator (Replaces the ontents of an existing object with aothher objects content)
-		Fixed& operator= (const Fixed& fixed);
+		Fixed& operator=(const Fixed& fixed);
 		// Destructor
 		~Fixed();
 
 		// Returns value of the fixed-point value
-		int getRawBits( void ) const;
+		// int getRawBits( void ) const;
 
-		// Sets teg raw value of the fixed-point number
-		void setRaBits( int const raw );
+		// // Sets teg raw value of the fixed-point number
+		// void setRawBits( int const raw );
 };
 
 #endif
