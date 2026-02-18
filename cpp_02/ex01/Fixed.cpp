c902e3584b6424d27fec4bdf0380c34b0003e62a
Waltteri Heino <waltteriheino@gmail.com>
2026-02-18 17:12:43 +0200

    Mod02/ex01 - Continuation of ex00, copying files as they are

---
+++ cpp_02/ex01/Fixed.cpp
@@ -0,0 +1,46 @@
+#include "Fixed.hpp"
+#include <iostream>
+
+/* Default Constructor
+	- Initializes the Fixed object wtih a fixed-point value of 0.
+*/
+Fixed::Fixed() : _value(0) {
+	std::cout << "Default constructor called\n";
+}
+
+/* Copy Constructor
+	- Creates a new Fixed object by copying 
+	  the internal '_value' from another Fixed object.
+*/
+Fixed::Fixed(const Fixed &copy) : _value(copy._value){
+	std::cout << "Copy constructor called\n";
+}
+
+/* Copy Assignment Operator ( = operator overload )
+	- Assigns the value of one Fixed object to another existing Fixed object.
+	- Includes a self-assignment check to avoid unnecessary work.
+*/
+Fixed& Fixed::operator=(const Fixed& fixed) {
+	std::cout << "Copy assignemnt operator called\n";
+	// Self assignmnet guard
+	if (this != &fixed) {
+		this->_value = fixed._value;
+	}
+	return *this;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called\n";
+}
+
+/* Sets the raw fixed-point value stored in the object */
+void Fixed::setRawBits( int const raw ) {
+	std::cout << "setRawBits member function called\n";
+	_value = raw;
+}
+
+/* Returns the raw fixed-point value stored in the object */
+int Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits member function called\n";
+	return (_value);
+}
---
+++ cpp_02/ex01/Fixed.hpp
@@ -0,0 +1,29 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+// Stores a number in fixed-point representation.
+class Fixed {
+	private:
+		int 				_value;
+		static const int	_fracBits = 8;
+	public:
+		// Default constructor
+		Fixed();
+		
+		// Copy constructor (Creates a new object as a copy of an existing one)
+		Fixed(const Fixed &copy);
+
+		//Copy assignment operator (Replaces the ontents of an existing object with aothher objects content)
+		Fixed& operator=(const Fixed& fixed);
+
+		// Destructor
+		~Fixed();
+
+		// Returns the fixed-point value exactly as it is stored.
+		int getRawBits( void ) const;
+
+		// Sets the raw value of the fixed-point number
+		void setRawBits( int const raw );
+};
+
+#endif
