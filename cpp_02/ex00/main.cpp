34db3b84ce6bc6c339fbd295e2bb27a00871733d
Waltteri Heino <waltteriheino@gmail.com>
2026-02-18 16:25:10 +0200

    Mod02/ex00 - Added comments and organized code

--- cpp_02/ex00/main.cpp
+++ cpp_02/ex00/main.cpp
@@ -2,10 +2,19 @@
 #include "Fixed.hpp"
 
 int main( void ) {
+	std::cout << "1.----\n";
 	Fixed a;
+	
+	std::cout << "2.----\n";
 	Fixed b( a );
+	
+	std::cout << "3.----\n";
 	Fixed c;
+	
+	std::cout << "4.----\n";
 	c = b;
+	
+	std::cout << "\nPrinting values:\n";
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
