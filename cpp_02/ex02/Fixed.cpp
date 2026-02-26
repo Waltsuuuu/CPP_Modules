137b425d9cd04afd42e9e686ada2bed985105a5e
Waltteri Heino <waltteriheino@gmail.com>
2026-02-26 14:28:15 +0200

    Mod02/ex02 - Added previous ex files and implemented multiplication operator overload

---
+++ cpp_02/ex02/Fixed.cpp
@@ -0,0 +1,100 @@
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+/* Default Constructor
+	- Initializes the Fixed object wtih a fixed-point value of 0.
+*/
+Fixed::Fixed() : _value(0) {
+	std::cout << "Default constructor called\n";
+}
+
+/* Integer Constructor
+	- Converts an integer into a fixed-point representation.
+	- Left shift bits by 8 (_fracBits) to make room for fractional part.
+*/
+Fixed::Fixed(int input) : _value(input << _fracBits) {
+	std::cout << "Integer constructor called\n";
+}
+
+/* Float Constructor
+	- Converts a float into a fixed-point representation.
+	- Multiplies the float (input) by 2^_fracBits to scale it.
+	- Rounds the result to the nearest integer.
+	- Stores the result as the fixed-point value.
+*/
+Fixed::Fixed(float input) {
+	std::cout << "Float constructor called\n";
+	float scaled = input * (1 << _fracBits);
+	_value = static_cast<int>(roundf(scaled));
+}
+
+/* Copy Constructor
+	- Creates a new Fixed object by copying 
+	  the internal '_value' from another Fixed object.
+*/
+Fixed::Fixed(const Fixed &copy) : _value(copy._value){
+	std::cout << "Copy constructor called\n";
+}
+
+/* Fixed Object Destructor*/
+Fixed::~Fixed() {
+	std::cout << "Destructor called\n";
+}
+
+// -------------- MEMBER FUNCTIONS
+
+/* Converts the fixed-point value to a float representation
+*/
+float Fixed::toFloat( void ) const {
+	return (static_cast<float>(_value) / (1 << _fracBits));
+}
+
+/* Converts the fixed-point value to an integer representation 
+   by removing the fractional bits.
+	- Right shift bits by 8 (_fracBits), truncating the fractional part.
+*/
+int Fixed::toInt( void ) const {
+	return (_value >> _fracBits);
+}
+
+/* Sets the raw fixed-point value stored in the object */
+void Fixed::setRawBits( int const raw ) {
+	std::cout << "setRawBits member function called\n";
+	_value = raw;
+}
+
+/* Returns the raw fixed-point value stored in the object */
+int Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits member function called\n";
+	return (_value);
+}
+
+// -------------- OPERATOR OVERLOAD
+
+/* Copy Assignment Operator ( = operator overload )
+	- Assigns the value of one Fixed object to another existing Fixed object.
+	- Includes a self-assignment check to avoid unnecessary work.
+*/
+Fixed& Fixed::operator=(const Fixed& fixed) {
+	std::cout << "Copy assignment operator called\n";
+	// Self assignment guard
+	if (this != &fixed) {
+		this->_value = fixed._value;
+	}
+	return (*this);
+}
+
+/* << Insertion operator overload.
+	- Takes a reference to a std::ostream object and a reference to the Fixed object value.
+	- Converts Fixed object to a float and inserts it into the output stream.
+	- Return ostream object (which now includes the fixed-point value).
+*/
+std::ostream& operator<<(std::ostream& out, const Fixed& value) {
+	out << value.toFloat();
+	return (out);
+}
+
+Fixed Fixed::operator * (const Fixed& other) {
+	return Fixed(this->toFloat() * other.toFloat());
+}
---
+++ cpp_02/ex02/Fixed.hpp
@@ -0,0 +1,43 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+#include <ostream>
+
+class Fixed {
+	private:
+		int 				_value;
+		static const int	_fracBits = 8;
+	public:
+	// Constructors
+		Fixed();
+		Fixed(int input);
+		Fixed(float input);
+		Fixed(const Fixed &copy);
+		
+	// Destructor
+		~Fixed();
+
+	// '=' Operator overload.
+		Fixed& operator = (const Fixed& fixed);
+		Fixed& operator << (const Fixed& fixed);
+
+	// '*' Operator overload.
+		Fixed operator * (const Fixed& fixed);
+
+	// Returns the '_value' exactly as it is stored.
+		int getRawBits( void ) const;
+
+	// Sets the '_value' of the fixed-point number.
+		void setRawBits( int const raw );
+
+	// Converts the fixed-point '_value' to an integer representation.
+		int toInt( void ) const;
+
+	// Converts the fixed-point '_value' to a float representation.
+		float toFloat( void ) const;
+
+};
+
+// '<<' insertion operator overload.
+std::ostream& operator << (std::ostream& out, const Fixed& value);
+
+#endif
