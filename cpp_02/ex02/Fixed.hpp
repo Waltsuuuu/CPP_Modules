89d014c1974b84f37aa15eaf3e50b6a2fd8fc90b
Waltteri Heino <waltteriheino@gmail.com>
2026-03-03 12:07:30 +0200

    Mod02/ex02 - Code formatting

--- cpp_02/ex02/Fixed.hpp
+++ cpp_02/ex02/Fixed.hpp
@@ -16,52 +16,30 @@ class Fixed {
 	// Destructor
 		~Fixed();
 
-	// '=' Operator overload.
-		Fixed& operator = (const Fixed& fixed);
-		Fixed& operator << (const Fixed& fixed);
-
-	// '*' Operator overload.
-		Fixed operator * (const Fixed& other);
-
-	// '+' Operator overload.
-		Fixed operator + (const Fixed& other);
-
-	// '-' Operator overload.
-		Fixed operator - (const Fixed& other);
-
-	// '/' Operator overload.
-		Fixed operator / (const Fixed& other);
-
-	// '>' Operator overload.
-		bool operator > (const Fixed& other) const;
-	
-	// '>=' Operator overload.
-		bool operator >= (const Fixed& other) const ; 
-
-	// '<' Operator overload.
-		bool operator < (const Fixed& other) const;
-
-	// '<=' Operator overload.
-		bool operator <= (const Fixed& other) const;
-
-	// '==' Operator overload.
-		bool operator == (const Fixed& other) const;
-
-	// '!=' Operator overload.
-		bool operator != (const Fixed& other) const;
+	// Operator overload.
+		Fixed&	operator = (const Fixed& fixed);
+		Fixed	operator * (const Fixed& other);
+		Fixed	operator + (const Fixed& other);
+		Fixed	operator - (const Fixed& other);
+		Fixed	operator / (const Fixed& other);
+		bool	operator > (const Fixed& other) const;
+		bool	operator >= (const Fixed& other) const ; 
+		bool	operator < (const Fixed& other) const;
+		bool	operator <= (const Fixed& other) const;
+		bool	operator == (const Fixed& other) const;
+		bool	operator != (const Fixed& other) const;
 
 	// Returns the '_value' exactly as it is stored.
-		int getRawBits( void ) const;
+		int		getRawBits( void ) const;
 
 	// Sets the '_value' of the fixed-point number.
-		void setRawBits( int const raw );
+		void	setRawBits( int const raw );
 
 	// Converts the fixed-point '_value' to an integer representation.
-		int toInt( void ) const;
+		int		toInt( void ) const;
 
 	// Converts the fixed-point '_value' to a float representation.
-		float toFloat( void ) const;
-
+		float	toFloat( void ) const;
 };
 
 // '<<' insertion operator overload.
--- cpp_02/ex02/main.cpp
+++ cpp_02/ex02/main.cpp
@@ -2,22 +2,22 @@
 #include "Fixed.hpp"
 
 int main( void ) {
-	Fixed a;
-	Fixed const b( Fixed( 5.0f ) * Fixed( 2 ) );
-	std::cout << b << std::endl;
-	a = 30;
-	std::cout << a + b << std::endl;
-	std::cout << a - b << std::endl;
-	std::cout << a / b << std::endl;
-	if (a > b)
-		std::cout << "a is greater than b" << std::endl;
-	if (b < a)
-		std::cout << "b is less than a" << std::endl;
-	if (a != b)
-		std::cout << "a is NOT equal to b" << std::endl;
-	a = 10;
-	if (a == b)
-		std::cout << "a is equal to b" << std::endl;
+	// Fixed const b( Fixed( 5.0f ) * Fixed( 2 ) );
+	// std::cout << b << std::endl;
+	Fixed a(1.5f);
+	std::cout << a.getRawBits() << std::endl;
+	// std::cout << a + b << std::endl;
+	// std::cout << a - b << std::endl;
+	// std::cout << a / b << std::endl;
+	// if (a > b)
+	// 	std::cout << "a is greater than b" << std::endl;
+	// if (b < a)
+	// 	std::cout << "b is less than a" << std::endl;
+	// if (a != b)
+	// 	std::cout << "a is NOT equal to b" << std::endl;
+	// a = 10;
+	// if (a == b)
+	// 	std::cout << "a is equal to b" << std::endl;
 	// std::cout << ++a << std::endl;
 	// std::cout << a << std::endl;
 	// std::cout << a++ << std::endl;
