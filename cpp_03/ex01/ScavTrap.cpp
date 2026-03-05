4fb1b68d11b3efc66dc3cdbfdd744c741d1c57e0
Waltteri Heino <waltteriheino@gmail.com>
2026-03-05 17:30:20 +0200

    Mod03/ex01 - ScavTrap constuctors and deconstuctors added

--- cpp_03/ex01/ScavTrap.cpp
+++ cpp_03/ex01/ScavTrap.cpp
@@ -1,5 +1,31 @@
 #include "ScavTrap.hpp"
+#include <iostream>
 
-ScavTrap::ScavTrap() {
-	
+ScavTrap::ScavTrap() : ClapTrap() {
+	_hitPoints = 100;
+	_energyPoints = 50;
+	_attackDamage = 20;
+	std::cout << "ScavTrap named " << _name << " constructed" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name) {
+	_hitPoints = 100;
+	_energyPoints = 50;
+	_attackDamage = 20;
+	std::cout << "ScavTrap named " << _name << " constructed" << std::endl;
+}
+
+ScavTrap::~ScavTrap() {
+	std::cout << "ScavTrap named " << _name << " destructed" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap& copy) : ClapTrap(copy) {
+	std::cout << "ScavTrap " << _name << " copy constructed" << std::endl;
+}
+
+ScavTrap& ScavTrap::operator = (const ScavTrap& other) {
+	if (this != &other) {
+		ClapTrap::operator = (other);
+	}
+	return (*this);
 }
