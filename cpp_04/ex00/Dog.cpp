62a34662c6f8da9cf9b99124188d75edac4b120a
Waltteri Heino <waltteriheino@gmail.com>
2026-03-06 17:50:50 +0200

    Mod04/ex00 - Orthodox Canonical Form for Cat, Dog and WrongCat classes:

--- cpp_04/ex00/Dog.cpp
+++ cpp_04/ex00/Dog.cpp
@@ -5,6 +5,16 @@ Dog::Dog() : Animal() {
 	_type = "Dog";
 }
 
+Dog::Dog(const Dog& copy) : Animal(copy) {
+}
+
+Dog& Dog::operator = (const Dog& other) {
+	if (this != &other) {
+		Animal::operator = (other);
+	}
+	return (*this);
+}
+
 void Dog::makeSound() const {
 	std::cout << "Woof woof" << std::endl;
 }
--- cpp_04/ex00/WrongCat.hpp
+++ cpp_04/ex00/WrongCat.hpp
@@ -6,7 +6,8 @@
 class WrongCat : public WrongAnimal {
 	public:
 		WrongCat();
-
+		WrongCat(const WrongCat& copy);
+		WrongCat& operator = (const WrongCat& other);
 		void makeSound() const;
 };
 
