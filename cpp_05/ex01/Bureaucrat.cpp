fb5f4a0dff2f29e6cfd1c2dda8363d629f553d15
Waltteri Heino <waltteriheino@gmail.com>
2026-03-24 14:22:43 +0200

    Mod05/ex01 - Files from previous ex

---
+++ cpp_05/ex01/Bureaucrat.cpp
@@ -0,0 +1,64 @@
+#include "Bureaucrat.hpp"
+#include <string>
+
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name), _grade(grade) {
+	if (_grade < 1) {
+		throw GradeTooHighException();
+	}
+	if (_grade > 150) {
+		throw GradeTooLowException();
+	}
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& copy) : _name(copy._name), _grade(copy._grade){
+}
+
+Bureaucrat& Bureaucrat::operator = (const Bureaucrat& other) {
+	if (this != &other) {
+		this->_grade = other._grade;
+	}
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat() {
+}
+
+// --- Member Functions ---
+
+const std::string Bureaucrat::getName() const {
+	return (_name);
+}
+
+int	Bureaucrat::getGrade() const {
+	return (_grade);
+}
+
+void	Bureaucrat::incrementGrade()  {
+	if (_grade <= 1) {
+		throw GradeTooHighException();
+	}
+	_grade--;
+}
+
+void	Bureaucrat::decrementGrade() {
+	if (_grade >= 150) {
+		throw GradeTooLowException();
+	}
+	_grade++;
+}
+
+// --- Exceptions --- 
+
+const char* Bureaucrat::GradeTooHighException::what() const noexcept {
+	return ("Bureaucrat grade is too high");
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const noexcept {
+	return ("Bureaucrat grade is too low");
+}
+
+// --- Insertion Operator Overload ---
+std::ostream& operator << (std::ostream& out, const Bureaucrat& bureaucrat) {
+	out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
+	return out;
+}
