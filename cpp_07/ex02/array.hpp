0abb538f39dca10044181172db055f7ffc5c2040
Waltteri Heino <waltteriheino@gmail.com>
2026-08-27 14:05:03 +0300

    Mod07/ex02 - Array class header

---
+++ cpp_07/ex02/array.hpp
@@ -0,0 +1,26 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+// Generic array class.
+// Array<int> numbers(5) -- Makes an array that holds 5 ints.
+// Array<std::string> names(5) -- Makes an array that holds 5 strings.
+template <typename T>
+class Array {
+	private:
+		T* _data;
+		unsigned int _size;
+
+	public:
+		Array();								// Empty array
+		Array(unsigned int n);					// Arrat with 'n' elements
+		Array(const Array &other);				// Copy constructor
+		Array &operator=(const Array& other);	// Assignment operator
+		~Array();								// Destructor (frees alloced mem)
+
+		T &operator[](unsigned int index);				// Access array elements (ex. arr[0])
+		const T &operator[](unsigned int index) const;	// Indexing a const Array
+
+		unsigned int size() const				// Returns _size
+};
+
+#endif
