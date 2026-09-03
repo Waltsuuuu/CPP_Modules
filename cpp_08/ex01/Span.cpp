3868f356d7cb24ef26a37be2babd585f5184409a
Waltteri Heino <waltteriheino@gmail.com>
2026-09-03 16:23:08 +0300

    Mod08/ex01 - Added copy constructor implementation to Span class

--- cpp_08/ex01/Span.cpp
+++ cpp_08/ex01/Span.cpp
@@ -6,6 +6,8 @@ Span::Span() : _maxSize(0) {}
 
 Span::Span(unsigned int N) : _maxSize(N) {}
 
+Span::Span(const Span& other) : _maxSize(other._maxSize), _numbers(other._numbers) {}
+
 Span& Span::operator=(const Span& other) {
 	if (this != &other) {
 		_maxSize = other._maxSize;
