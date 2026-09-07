fb22ca8a8fca10f168db76cbf6e2485846c1a6c0
Waltteri Heino <waltteriheino@gmail.com>
2026-09-07 13:03:55 +0300

    Mod08/ex02 - MutantStack iterator definitions

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -6,6 +6,7 @@
 template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container> {
 	public:
+		// OCF
 		MutantStack() : std::stack<T, Container>() {}; 
 		MutantStack(const MutantStack& other) : std::stack<T, Container>(other) {};
 		MutantStack& operator=(const MutantStack& other) {
@@ -14,6 +15,13 @@ class MutantStack : public std::stack<T, Container> {
 			return *this;
 		}
 		~MutantStack() {};
+
+		// Iterators
+		typedef typename Container::iterator				iterator;
+		typedef typename Container::const_iterator			const_iterator;
+		typedef typename Container::reverse_iterator		reverse_iterator;
+		typedef typename Container::const_reverse_iterator	const_reverse_iterator;
+			
 	private:
 };
 
