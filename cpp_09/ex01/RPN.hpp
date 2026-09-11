9cf2433566cf85a10ae2d0a7eb84e7fb33580969
Waltteri Heino <waltteriheino@gmail.com>
2026-09-11 15:38:59 +0300

    Mod09/ex01 - RPN OCF

---
+++ cpp_09/ex01/RPN.hpp
@@ -0,0 +1,19 @@
+#ifndef RPN_HPP
+# define RPN_HPP
+
+#include <stack>
+#include <string>
+
+class RPN {
+	private:
+		std::stack<int> _stack;
+
+
+	public:
+		RPN();
+		RPN(const RPN& other);
+		RPN& operator=(const RPN& other);
+		~RPN();
+};
+
+#endif
